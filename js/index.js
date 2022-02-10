626b577d839a9fe6949ee679cc0fbc36cb4fa931
Jose Alejandro Lopez <emp.joselopez@gmail.com>
2022-02-10 19:55:50 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,25 +1,5 @@
 window.addEventListener('load', () => {
-   var semana = ['dilluns','dimarts','dimecres','dijous','divendres'];
-   console.log(semana[semana.length-4]);
-
-    if(5 == '5') {
-        console.log('Estás en lo correcto :]');
-    }
-
-    const a = [1,3,5,7,9];
-    const b = [1,3,5,7,9];
-    var i = 0;
-
-    while(a[i] === b[i] && a.length === b.length && a[i] != undefined) {
-        i++;
-    }
-    
-    if(i === a.length) {
-        console.log('true');
-    }else {
-        console.log('False');
-    }
-
+   
 /*     let callMeButton = document.getElementById('call-me-button');
     
     callMeButton.addEventListener('click', (ev) => {
